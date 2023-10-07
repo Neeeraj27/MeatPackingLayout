85813ef94b5d1e7ff509a472ccfc4091fd458823
Neeeraj27 <neerajrautela88@gmail.com>
2023-10-07 22:40:01 +0530

    Final Commit

---
+++ app/src/main/java/com/example/pro/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.pro
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pro.databinding.ActivityMainBinding
+
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
